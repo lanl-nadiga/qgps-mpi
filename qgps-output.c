a08e2d2663933454984c978c08b0579f333434dd
Nick Howell <nlhowell@gmail.com>
2011-07-21 01:07:30 -0500

    factor output code, for arbitrary fields

--- qgps-output.c
+++ qgps-output.c
@@ -2,10 +2,8 @@
 #include <mpi.h>
 #include "qgps-mpi.h"
 
-MPI_File qgps_output_file;
-
 int qgps_output() {
-        return qgps_output_open() || qgps_output_write() || qgps_output_close();
+        qgps_output_complex(qgps_output_filename(), omega);
 }
 
 char *qgps_output_filename() {
@@ -19,27 +17,42 @@ char *qgps_output_filename() {
         return s;
 }
 
-int qgps_output_open() {
-        MPI_File_open(QGPS_COMM_WORLD, qgps_output_filename(),
+MPI_File qgps_output_open_file(char *filename) {
+        MPI_File file;
+        MPI_File_open(QGPS_COMM_WORLD, filename,
                         MPI_MODE_CREATE | MPI_MODE_WRONLY,
-                        MPI_INFO_NULL, &qgps_output_file);
+                        MPI_INFO_NULL, &file);
 
-        MPI_File_set_view(qgps_output_file, qgps_current_real_block->y_begin * qgps_nx,
+        MPI_File_set_view(file, qgps_current_real_block->y_begin * qgps_nx,
                         MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
+        return file;
 }
 
-int qgps_output_close() {
-        return MPI_File_close(&qgps_output_file);
+int qgps_output_close_file(MPI_File *file) {
+        return MPI_File_close(file);
 }
 
-int qgps_output_write() {
-        static double *omega_real = NULL;
+int qgps_output_real(char *filename, double *data) {
+        MPI_File file = qgps_output_open_file(filename);
+        qgps_output_write_real(file, data);
+        qgps_output_close_file(&file);
+}
+int qgps_output_complex(char *filename, complex *data) {
+        MPI_File file = qgps_output_open_file(filename);
+        qgps_output_write_complex(file, data);
+        qgps_output_close_file(&file);
+}
+int qgps_output_write_complex(MPI_File file, complex *data) {
+        static double *data_real = NULL;
 
-        if (!omega_real)
-                omega_real = fftw_alloc_real(qgps_local_size * 2);
+        if (!data_real)
+                data_real = fftw_alloc_real(qgps_local_size * 2);
 
-        qgps_dft_c2r(omega, omega_real);
-        qgps_transpose_r(omega_real);
+        qgps_dft_c2r(data, data_real);
+        qgps_output_write_real(file, data_real);
+}
+int qgps_output_write_real(MPI_File file, double *data) {
+        qgps_transpose_r(data);
 
         qgps_block_t *b = qgps_current_real_block;
 
@@ -47,7 +60,7 @@ int qgps_output_write() {
         for (int j = b->y_begin; j < b->y_end; j++) {
                 int idx = (j - b->y_begin) * (qgps_nx + pad);
 
-                MPI_File_write(qgps_output_file, &omega_real[idx], qgps_nx,
+                MPI_File_write(file, &data[idx], qgps_nx,
                                 MPI_DOUBLE, MPI_STATUS_IGNORE);
         }
         return 0;
--- qgps-output.h
+++ qgps-output.h
@@ -1,10 +1,15 @@
 #ifndef _QGPS_OUTPUT_H
 #define _QGPS_OUTPUT_H
+#include <mpi.h>
 
-int qgps_output_open();
-int qgps_output_write();
-int qgps_output_close();
 int qgps_output();
+
+MPI_File qgps_output_open_file(char *filename);
+int qgps_output_close_file(MPI_File *file);
+int qgps_output_write_complex(MPI_File file, complex *data);
+int qgps_output_write_real(MPI_File file, double *data);
+int qgps_output_complex(char *filename, complex *data);
+int qgps_output_real(char *filename, double *data);
 
 char *qgps_output_filename();
 
